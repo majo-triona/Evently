f08e6ceace5af8795b1ecc070ad09a28b5d0768f
Teo <168961146+majo-triona@users.noreply.github.com>
2025-05-06 14:16:44 +0300

    Поправена грешка при добавяне на събития

--- events/views.py
+++ events/views.py
@@ -1,15 +1,47 @@
-import calendar
+from django.shortcuts import render, redirect
+from .models import Event
 from datetime import date
 from calendar import HTMLCalendar
-from django.shortcuts import render
-from .models import Event
+from datetime import datetime
+
+class EventCalendar(HTMLCalendar):
+    def __init__(self, events):
+        super().__init__()
+        self.events_by_date = self.group_by_day(events)
+
+    def group_by_day(self, events):
+        data = {}
+        for event in events:
+            day = event.date.day
+            if day in data:
+                data[day].append(event)
+            else:
+                data[day] = [event]
+        return data
+
+    def formatday(self, day, weekday):
+        if day == 0:
+            return '<td></td>'  # празна клетка
+        cssclass = self.cssclasses[weekday]
+        events = self.events_by_date.get(day, [])
+        badge = f'<span class="badge">{len(events)}</span>' if events else ''
+        return f'<td class="{cssclass}"><div>{day}{badge}</div></td>'
 
 def calendar_view(request):
     today = date.today()
     year = today.year
     month = today.month
-    cal = HTMLCalendar().formatmonth(year, month)
+
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        event_date = request.POST.get("date")
+        if title and event_date:
+            Event.objects.create(title=title, description=description, date=event_date)
+            return redirect('/')
+
     events = Event.objects.filter(date__month=month)
+    cal = EventCalendar(events).formatmonth(year, month)
     return render(request, 'events/calendar.html', {
         'calendar': cal,
         'events': events,
