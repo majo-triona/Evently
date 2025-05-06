6b5863d1b8fd810962115f79b924ca4de3c84cad
Teo <168961146+majo-triona@users.noreply.github.com>
2025-05-06 12:53:17 +0300

    Преглед на календар

--- events/views.py
+++ events/views.py
@@ -1,3 +1,16 @@
+import calendar
+from datetime import date
+from calendar import HTMLCalendar
 from django.shortcuts import render
+from .models import Event
 
-# Create your views here.
+def calendar_view(request):
+    today = date.today()
+    year = today.year
+    month = today.month
+    cal = HTMLCalendar().formatmonth(year, month)
+    events = Event.objects.filter(date__month=month)
+    return render(request, 'events/calendar.html', {
+        'calendar': cal,
+        'events': events,
+    })
