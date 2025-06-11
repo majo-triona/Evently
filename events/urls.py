ac7b54fc246e592ef6d901adda28f142356856ac
Teo <168961146+majo-triona@users.noreply.github.com>
2025-06-11 12:28:38 +0300

    Поправени грешки при urls.py

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('events/', views.event_list_create, name='event-list-create'),
+    path('', views.calendar_view),
+    path('api/', views.event_list_create),
 ]
